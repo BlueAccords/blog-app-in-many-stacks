70a8c54e8c448822be1744ccfafd2640e0b3e1bf
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-12-08 02:20:27 -0500

    updated the readability and comment of the tag-controller.

--- back-ends/nodejs-rest/app/controllers/tags-controller.js
+++ back-ends/nodejs-rest/app/controllers/tags-controller.js
@@ -2,9 +2,16 @@
 
 import Tag from './../models/Tag';
 
+// Find all posts that contain a specific tag.
 module.exports.findAllPostWithTag = (req, res) => {
-  Tag.find({ text: req.params.name.toLowerCase() }, (err, tag) => {
-    if (tag === null) { req.send('No post with this tag name found'); }
-    else { res.json(tag); }
+  Tag.find({
+    text: req.params.name.toLowerCase(),
+  }, (err, tag) => {
+    if (tag === null) {
+      req.send('No post with this tag name found');
+    }
+    else {
+      res.json(tag);
+    }
   });
 };
