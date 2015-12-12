8013ebd288a97b7dba30fc2069c368b77d3bd0c7
Rosel Padilla <roselpadilla.mail@gmail.com>
2015-12-11 19:39:15 -0500

    Added url_path to posts

--- back-ends/nodejs-rest/app/controllers/posts-controller.js
+++ back-ends/nodejs-rest/app/controllers/posts-controller.js
@@ -5,9 +5,10 @@ import Post from './../models/Post';
 module.exports.create = (req, res) => {
   let now = new Date();
   let path = req.body.title.toLowerCase().split(' ').join('-');
+  let author = req.user.username;
 
   Post.create({
-    url_path: path,
+    url_path: path + '-' + author,
     title: req.body.title,
     body: req.body.body,
     user_id: req.user._id,
