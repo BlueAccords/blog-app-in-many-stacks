c6b741a95dd087827c0aa21fa64bbf169d130e69
Chiedo <chiedo@chie.do>
2015-12-18 14:38:07 -0500

    Got create post mutation updated

--- back-ends/nodejs-graphql/app/mutations/create-post-mutation.js
+++ back-ends/nodejs-graphql/app/mutations/create-post-mutation.js
@@ -1,6 +1,7 @@
 import Post from '../models/Post';
-import postType from '../types/post-type.js';
+import userType from '../types/user-type';
 import { Promise } from 'es6-promise';
+import { postEdge } from '../connection-definitions/post-connection-definitions';
 
 import {
   GraphQLNonNull,
@@ -9,6 +10,7 @@ import {
 
 import {
   mutationWithClientMutationId,
+  cursorForObjectInConnection,
 } from 'graphql-relay';
 
 let createPostMutation = new mutationWithClientMutationId({
@@ -19,11 +21,30 @@ let createPostMutation = new mutationWithClientMutationId({
     body: {type: new GraphQLNonNull(GraphQLString)},
   },
   outputFields: {
-    post: {
-      type: postType,
+    user: {
+      type: userType,
       resolve: (data) => {
-        return Post.findOne({'_id': data.postId})
-        .then((post) => post);
+        return data.user;
+      },
+    },
+    postEdge: {
+      type: postEdge,
+      resolve: (data) => {
+        let postId = data.postId;
+        return Post.find({'_id': data.postId})
+        .then((posts) => {
+          let post;
+          posts.forEach((tmpPost) => {
+            if(tmpPost._id.toString() === postId.toString()) {
+              post = tmpPost;
+            }
+          });
+
+          return {
+            cursor: cursorForObjectInConnection(posts, post),
+            node: post,
+          };
+        });
       },
     },
   },
