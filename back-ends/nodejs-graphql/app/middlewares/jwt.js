4f182c86608a10f724979df9d73906c971bb4807
Chiedo <chiedo@chie.do>
2015-12-18 13:35:59 -0500

    Improved JWT middleware

--- back-ends/nodejs-graphql/app/middlewares/jwt.js
+++ back-ends/nodejs-graphql/app/middlewares/jwt.js
@@ -24,9 +24,10 @@ module.exports = function(req, res, next) {
 
     return jwt.verify(token, config.jwt.secret, {ignoreExpiration: ignoreExpiration}, (err, decoded) => {
       if(decoded) {
-        return User.findOne({'_id': decoded._id})
+        return User.findOne({'_id': decoded._id, 'password': decoded.password, 'email': decoded.email })
         .then((user) => {
           req.currentUser = user;
+          req.authToken = token;
           return next();
         });
       } else {
