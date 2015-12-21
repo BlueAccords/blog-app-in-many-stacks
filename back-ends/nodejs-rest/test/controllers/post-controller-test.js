7051411ef0028b87c060bee5dc93ce3a1caf5852
Chiedo <chiedo@chie.do>
2015-12-21 14:34:36 -0500

    More stubs

---
+++ back-ends/nodejs-rest/test/controllers/post-controller-test.js
@@ -0,0 +1,20 @@
+/* eslint no-undef: 0*/
+import { createDB, destroyDB } from '../test-helper';
+//import app from '../../app';
+
+describe('Posts:', () => {
+  describe('Create', () => {
+    xit('should allow a user to create a post');
+    xit('should not allow someone with no account to create a post');
+  });
+
+  describe('Update', () => {
+    xit('should allow a user to update a post he owns');
+    xit('should allow a user to update a post he owns');
+  });
+
+  describe('Delete', () => {
+    xit('should not allow a user to delete a post he does not own');
+    xit('should not allow a user to delete a post he does not own');
+  });
+});
--- back-ends/nodejs-rest/test/test-helper.js
+++ back-ends/nodejs-rest/test/test-helper.js
@@ -1,7 +1,15 @@
 let mongoose = require('mongoose');
 let mockgoose     = require('mockgoose');
+import {Promise} from 'es6-promise';
 
-module.exports.setupDB = (done) => {
+module.exports.createDB = (done) => {
   mockgoose(mongoose);
   mongoose.connect('mongodb://localhost/test', done);
+  return Promise.resolve();
+};
+
+module.exports.destroyDB = () => {
+  mockgoose(mongoose);
+  mongoose.connection.close();
+  return Promise.resolve();
 };
